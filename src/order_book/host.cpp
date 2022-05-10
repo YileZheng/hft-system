be11c65ded665cdb654eec013185b63fde0c7a8a
Leon8 <zhyile@outlook.com>
2022-05-10 21:45:11 +0800

    host files coding

--- src/order_book/host.cpp
+++ src/order_book/host.cpp
@@ -8,6 +8,8 @@
 #include <limits.h>
 #include <sys/stat.h>
 
+#include "common.hpp"
+#include "host_message.hpp"
 
 static const std::string error_message =
     "Error: Result mismatch:\n"
@@ -93,9 +95,15 @@ char* read_binary_file(const std::string &xclbin_file_name, unsigned &nb)
 };
 
 
+char symbols[STOCK_TEST][8] =  {{' ',' ',' ',' ','L','P', 'A','A'},
+								{' ',' ',' ',' ', 'N','Z','M','A'},
+								{' ',' ',' ',' ', 'G','O','O','G'}};
+// config
+symbol_t *symbol_map=(symbol_t*)symbols;
+
 
 int main(int argc, char* argv[]) {
-
+	{ // device, context, command queue, binary file, program
 	const char* xclbinFilename;
 
 	if (argc==2) {
@@ -103,19 +111,9 @@ int main(int argc, char* argv[]) {
 		std::cout <<"Using FPGA binary file specfied through the command line: " << xclbinFilename << std::endl;
 	}
 	else {
-		xclbinFilename = "../binary_container_1.xclbin";
+		xclbinFilename = "../order_book.xclbin";
 		std::cout << "No FPGA binary file specified through the command line, using:" << xclbinFilename <<std::endl;
 	}
-	 
-    // Compute the size of array in bytes
-    size_t size_in_bytes = DATA_SIZE * sizeof(int);
-    
-    // Creates a vector of DATA_SIZE elements with an initial value of 10 and 32
-    // using customized allocator for getting buffer alignment to 4k boundary
-    std::vector<int,aligned_allocator<int>> source_a(DATA_SIZE, 10);
-    std::vector<int,aligned_allocator<int>> source_b(DATA_SIZE, 32);
-    std::vector<int,aligned_allocator<int>> source_results(DATA_SIZE);
-    
 
     std::vector<cl::Device> devices = xcl::get_xil_devices();
     cl::Device device = devices[0];
@@ -140,65 +138,100 @@ int main(int argc, char* argv[]) {
     bins.push_back({buf,nb});
     cl::Program program(context, devices, bins);
     
+	}
+    
     // This call will get the kernel object from program. A kernel is an 
     // OpenCL function that is executed on the FPGA. 
-    cl::Kernel krnl_vector_add(program,"krnl_vadd");
-    
+    cl::Kernel krnl_vector_add(program,"order_book");
 	cl::Kernel krnl_const_add(program,"rtl_kernel_wizard_0");
 
-// streamming
+    cl::Kernel krnl_order_book(program,"order_book");
+	 
+	// Data preparation -----------------------------
 
-// Device connection specification of the stream through extension pointer
-cl_mem_ext_ptr_t  ext;  // Extension pointer
-ext.param = kernel;     // The .param should be set to kernel (cl_kernel type)
-ext.obj = nullptr;
+    // Compute the size of array in bytes
+    size_t size_in_bytes = DATA_SIZE * sizeof(int);
+    
+    // Creates a vector of DATA_SIZE elements with an initial value of 10 and 32
+    // using customized allocator for getting buffer alignment to 4k boundary
+    std::vector<int,aligned_allocator<int>> source_a(DATA_SIZE, 10);
+    std::vector<int,aligned_allocator<int>> source_b(DATA_SIZE, 32);
+    std::vector<int,aligned_allocator<int>> source_results(DATA_SIZE);
 
-// The .flag should be used to denote the kernel argument
-// Create write stream for argument 3 of kernel
-ext.flags = 0;
-cl_stream h2k_stream = clCreateStream(device_id, XCL_STREAM_READ_ONLY, CL_STREAM, &ext, &ret);
+	messageManager messages_handler;
 
-// Create read stream for argument 4 of kernel
-ext.flags = 1;
-cl_stream k2h_stream = clCreateStream(device_id, XCL_STREAM_WRITE_ONLY, CL_STREAM, &ext,&ret);
-// Set kernel non-stream argument (if any)
-clSetKernelArg(kernel, 0,...,...);
-clSetKernelArg(kernel, 1,...,...);
-clSetKernelArg(kernel, 2,...,...);
-// 3rd and 4th arguments are not set as those are already specified when creating the streams
+	map<string, long> symbol2hex = {{"AAPL", 0x4141504c20202020},
+									{"AMZN", 0x414d5a4e20202020},
+									{"GOOG", 0x474f4f4720202020},
+									{"INTC", 0x494e544320202020},
+									{"MSFT", 0x4d53465420202020},
+									{"SPY" , 0x5350592020202020},
+									{"TSLA", 0x54534c4120202020},
+									{"NVDA", 0x4e56444120202020},
+									{"AMD" , 0x414d442020202020},
+									{"QCOM", 0x51434f4d20202020}};
 
-// Schedule kernel enqueue
-clEnqueueTask(commands, kernel, . .. . );
+	symbol_t read_symbol = symbol2hex["AAPL"];
+	ap_uint<8> read_max = 10;
+	char instruction = 'A';
 
-// Initiate the READ transfer
-cl_stream_xfer_req rd_req {0};
 
-rd_req.flags = CL_STREAM_EOT | CL_STREAM_NONBLOCKING;
-rd_req.priv_data = (void*)"read"; // You can think this as tagging the transfer with a name
-clReadStream(k2h_stream, host_read_ptr, max_read_size, &rd_req, &ret);
+	// streamming ------------------------------
 
-// Initiating the WRITE transfer
-cl_stream_xfer_req wr_req {0};
+	// Device connection specification of the stream through extension pointer
+	cl_mem_ext_ptr_t  ext;  // Extension pointer
+	ext.param = krnl_order_book;     // The .param should be set to kernel (cl_kernel type)
+	ext.obj = nullptr;
 
-wr_req.flags = CL_STREAM_EOT | CL_STREAM_NONBLOCKING;
-wr_req.priv_data = (void*)"write";
+	// The .flag should be used to denote the kernel argument
+	// Create write stream for argument 0 of kernel
+	ext.flags = 0;
+	cl_stream h2k_stream = clCreateStream(device, XCL_STREAM_READ_ONLY, CL_STREAM, &ext, &ret);
 
-clWriteStream(h2k_stream, host_write_ptr, write_size, &wr_req , &ret);
+	// Create read stream for argument 1 of kernel
+	ext.flags = 1;
+	cl_stream k2h_stream = clCreateStream(device, XCL_STREAM_WRITE_ONLY, CL_STREAM, &ext,&ret);
 
-// Checking the request completion
-cl_streams_poll_req_completions poll_req[2] {0, 0}; // 2 Requests
 
-auto num_compl = 2;
-clPollStreams(device_id, poll_req, 2, 2, &num_compl, 5000, &ret);
-// Blocking API, waits for 2 poll request completion or 5000ms, whichever occurs first
+	// Set kernel non-stream argument (if any)
+	clSetKernelArg(krnl_order_book, 2, sizeof(symbol_t), 	&read_symbol);
+	clSetKernelArg(krnl_order_book, 3, sizeof(char), 		&read_max);		// 64 bits here instead of 8
+	clSetKernelArg(krnl_order_book, 4, sizeof(char), 		&instruction);
+	// 3rd and 4th arguments are not set as those are already specified when creating the streams
 
-for (auto i=0; i<2; ++i) {
-    if(rd_req.priv_data == poll_req[i].priv_data) { // Identifying the read transfer
+	clSetKernelArg(krnl_order_book, 4, sizeof(char), 		&instruction);
+	// // Schedule kernel enqueue
+	// clEnqueueTask(commands, kernel, . .. . );
 
-     // Getting read size, data size from kernel is unknown
-     ssize_t result_size=poll_req[i].nbytes;
-     }
-}
+	// Initiate the READ transfer
+	cl_stream_xfer_req rd_req {0};
+	rd_req.flags = CL_STREAM_EOT | CL_STREAM_NONBLOCKING;
+	rd_req.priv_data = (void*)"bookread"; // You can think this as tagging the transfer with a name
+	clReadStream(k2h_stream, host_read_ptr, max_read_size, &rd_req, &ret);
+
+	// Initiating the WRITE transfer
+	cl_stream_xfer_req wr_req {0};
+	wr_req.flags = CL_STREAM_EOT | CL_STREAM_NONBLOCKING;
+	wr_req.priv_data = (void*)"orderwrite";
+	clWriteStream(h2k_stream, host_write_ptr, write_size, &wr_req , &ret);
+
+
+	// Checking the request completion, wait until finished
+	cl_streams_poll_req_completions poll_req[2] {0, 0}; // 2 Requests
+
+	auto num_compl = 2;
+	clPollStreams(device, poll_req, 2, 2, &num_compl, 100000, &ret);
+	// Blocking API, waits for 2 poll request completion or 100'000ms, whichever occurs first
+
+	for (auto i=0; i<2; ++i) {
+		if(rd_req.priv_data == poll_req[i].priv_data) { // Identifying the read transfer
+
+		// Getting read size, data size from kernel is unknown
+		ssize_t result_size=poll_req[i].nbytes;
+		}
+	}
+	// ----------------------------------
+
 
     // These commands will allocate memory on the Device. The cl::Buffer objects can
     // be used to reference the memory locations on the device. 
@@ -246,4 +279,221 @@ for (auto i=0; i<2; ++i) {
     std::cout << "TEST WITH TWO KERNELS " << (match ? "FAILED" : "PASSED") << std::endl; 
     return (match ? EXIT_FAILURE :  EXIT_SUCCESS);
 
+}
+
+
+vector<string> split_string(
+	std::string s,
+	std::string delimiter
+	){
+
+	size_t pos = 0;
+	std::string token;
+	vector<string> res;
+	while ((pos = s.find(delimiter)) != std::string::npos) {
+		token = s.substr(0, pos);
+		res.push_back(token);
+		s.erase(0, pos + delimiter.length());
+	}
+	res.push_back(s);
+//	std::cout << "Splitted line length: " << res.size() << std::endl;
+	return res;
+}
+
+string concat_string(
+	vector<vector<price_depth>> pd, 
+	std::string delimiter,
+	int level
+	){
+	float multiple = MULTI;
+
+	std::ostringstream ss;
+	price_depth cur_pd; 
+	int price;
+	int size;
+
+	vector<vector<price_depth>::iterator> iter_v, end_v;
+	for (std::vector<vector<price_depth>>::iterator it = pd.begin(); it != pd.end(); ++it){
+		iter_v.push_back(it->begin());
+		end_v.push_back(it->end());
+	}
+
+	for (int i=0; i<level; i++){
+		for (int ii = 0; ii < iter_v.size(); ++ii){
+			if (iter_v[ii] < end_v[ii]){
+				cur_pd = *(iter_v[ii]++);
+				price = (std::round(((float)cur_pd.price)/UNIT)*(multiple*UNIT));
+				size = cur_pd.size;
+			}else{
+				price = (ii == 0)? -9999999999: 9999999999;
+				size = 0;
+			}
+			ss<< price <<delimiter<< size;
+			if (!((i == level-1) && (ii == iter_v.size()-1))){
+				ss << delimiter;
+			}
+		}
+	}
+
+	return std::string(ss.str());
+}
+
+
+void last_manager::check_update_last_price(
+	vector<string> orderBook_split,
+	Time tmstmp
+){
+	orderOp odop;
+	ap_uint<1> bid, req_read=0;
+	Message input_in;
+	vector<pair<int, int>> *cache;
+
+	int offset, vol_cur, price_cur, target_price;
+	// bid at the end
+	offset = 0;
+	price_cur = stoi(*(orderBook_split.end()-2-4*offset)); 
+	while (price_cur == -9999999999){
+		price_cur = stoi(*(orderBook_split.end()-2-4*(++offset)));
+		cache_lastb.clear();
+	}
+	vol_cur = stoi(*(orderBook_split.end()-1-4*offset));
+
+	cache = &cache_lastb;
+	if (price_cur < price_last_b){
+		int vol_diff;
+		bool br = false;
+		while (true){
+			if (cache->size() > cache_max_len){
+					vol_diff = - cache->front().second;
+					target_price = cache->front().first;
+					cache->erase(cache->begin());
+			}
+			else
+			{
+				if ((price_cur > cache->back().first) || (cache->size()==0)){
+					// expected price maybe still behind, add this unexpected new price
+					br = true;
+					vol_diff = vol_cur;
+					target_price = price_cur;
+				}else if (price_cur == cache->back().first){
+					// hit the expected price
+					br = true;
+					vol_diff = vol_cur - cache->back().second;
+					target_price = price_cur;
+					cache->pop_back();
+				}else{
+					// expected price is removed from the book already, remove it untill get a new reasonable expected price
+					vol_diff = - cache->back().second;
+					target_price = cache->back().first;
+					cache->pop_back();
+				}
+			}
+			
+			if (vol_diff != 0){
+				input_in.price = (price_t)((float)(target_price)/MULTI); input_in.size = (qty_t)(vol_diff); input_in.orderID = 1000;
+				bid = 1;
+				odop = (vol_diff<0)? CHANGE: NEW;
+				std::cout <<"Change on price level at the end: ";
+				std::cout<<" Symbol: " << cur_symbol <<" OrderID: "<<input_in.orderID << " Side: " <<bid<<" Type: "<<odop<<" Price: "<< input_in.price <<" Volume: "<< input_in.size <<" Read: "<<req_read<<endl;
+				input_in.timestamp = tmstmp;
+				input_in.symbol = cur_symbol;
+				input_in.operation = odop;
+				input_in.side = bid;
+				stream_in.write(input_in);
+				order_book_system(
+					// data
+					stream_in,
+					price_stream_out,
+					// configuration inputs
+					cur_symbol,
+					read_max,
+					// control input
+					'v'  // void, run, halt, read book, clear, config symbol map | read_max 
+				);
+			}
+			if (br) break;
+		}
+	}
+	else if (price_cur > price_last_b) // price at the end overflow
+	{
+		cache->push_back( make_pair(price_last_b, vol_last_b));
+		std::cout << "Price orderflow: "<< price_last_b << std::endl;
+	}
+	price_last_b = price_cur;
+	vol_last_b = vol_cur;
+	
+
+	// ask at the end
+	offset = 0;
+	price_cur = stoi(*(orderBook_split.end()-4-4*offset)); 
+	while (price_cur == 9999999999){
+		price_cur = stoi(*(orderBook_split.end()-4-4*(++offset)));
+		cache_lasta.clear();
+	}
+	vol_cur = stoi(*(orderBook_split.end()-3-4*offset));
+
+	cache = &cache_lasta;
+	if (price_cur > price_last_a){
+		int vol_diff;
+		bool br = false;
+		while (true){
+			if (cache->size() > cache_max_len){
+					vol_diff = - cache->front().second;
+					target_price = cache->front().first;
+					cache->erase(cache->begin());
+			}
+			else
+			{
+				if ((price_cur < cache->back().first) || (cache->size()==0)){
+					// expected price maybe still behind, add this unexpected new price
+					br = true;
+					vol_diff = vol_cur;
+					target_price = price_cur;
+				}else if (price_cur == cache->back().first){
+					// hit the expected price
+					br = true;
+					vol_diff = vol_cur - cache->back().second;
+					target_price = price_cur;
+					cache->pop_back();
+				}else{
+					// expected price is removed from the book already, remove it untill get a new reasonable expected price
+					vol_diff = - cache->back().second;
+					target_price = cache->back().first;
+					cache->pop_back();
+				}
+			}
+			
+			if (vol_diff != 0){
+				input_in.price = (price_t)((float)(target_price)/MULTI); input_in.size = (qty_t)(vol_diff); input_in.orderID = 1000;
+				bid = 0;
+				odop = (vol_diff<0)? CHANGE: NEW;
+				std::cout <<"Change on price level at the end: ";
+				std::cout<<" Symbol: " << cur_symbol <<" OrderID: "<<input_in.orderID << " Side: " <<bid<<" Type: "<<odop<<" Price: "<< input_in.price <<" Volume: "<< input_in.size <<" Read: "<<req_read<<endl;
+				input_in.timestamp = tmstmp;
+				input_in.symbol = cur_symbol;
+				input_in.operation = odop;
+				input_in.side = bid;
+				stream_in.write(input_in);
+				order_book_system(
+					// data
+					stream_in,
+					price_stream_out,
+					// configuration inputs
+					cur_symbol,
+					read_max,
+					// control input
+					'v'  // void, run, halt, read book, clear, config symbol map | read_max 
+				);
+			}
+			if (br) break;
+		}
+	}
+	else if (price_cur < price_last_a) // price at the end overflow
+	{
+		cache->push_back( make_pair(price_last_a, vol_last_a));
+		std::cout << "Price orderflow: "<< price_last_a << std::endl;
+	}
+	price_last_a = price_cur;
+	vol_last_a = vol_cur;
+	
 }
---
+++ src/order_book/host_message.hpp
@@ -0,0 +1,131 @@
+
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<vector>
+#include<map>
+#include "common.hpp"
+
+#define STOCK_TEST 3
+
+using namespace std;
+
+
+vector<string> split_string(std::string s,std::string delimiter);
+string concat_string(vector<vector<price_depth>> pd, std::string delimiter, int level);
+void check_update_last_price(vector<string> orderBook_split, int price_lasta_init, int price_lastb_init, int vol_lasta_init, int vol_lastb_init);
+
+class last_manager{
+	int cache_max_len = 10;
+	map<int, int> cache_last;
+	vector<pair<int, int>> cache_lasta, cache_lastb;
+	int price_last_b, price_last_a;
+	int vol_last_b, vol_last_a;
+	symbol_t cur_symbol;
+	
+
+	public:
+	last_manager(
+		int price_lasta_init,
+		int price_lastb_init,
+		int vol_lasta_init,
+		int vol_lastb_init,
+		symbol_t cur_symbol_init
+	){
+		price_last_b=price_lastb_init, price_last_a=price_lasta_init;
+		vol_last_b=vol_lastb_init, vol_last_a=vol_lasta_init;
+		cur_symbol = cur_symbol_init;
+	}
+
+	last_manager(last_manager &l){
+		price_last_b=l.price_last_b;
+		price_last_a=l.price_last_a;
+		vol_last_b=l.vol_last_b;
+		vol_last_a=l.vol_last_a;
+		cur_symbol = l.cur_symbol;
+
+	}
+
+	vector<Message> check_update_last_price(
+		vector<string> orderBook_split,
+		Time tmstmp
+	);
+
+};
+
+
+class messageManager{
+
+	int time_speedup = 1; // how many times faster to backtest, controls the message send rate
+	int level=10;
+
+	// files
+	string message_path[STOCK_TEST] =  {{"/home/yzhengbv/00-data/git/hft-system/data/lobster/subset/AAPL_2012-06-21_34200000_57600000_message_10.csv"},
+										{"/home/yzhengbv/00-data/git/hft-system/data/lobster/subset/AMZN_2012-06-21_34200000_57600000_message_10.csv"},
+										{"/home/yzhengbv/00-data/git/hft-system/data/lobster/subset/GOOG_2012-06-21_34200000_57600000_message_10.csv"}};
+	string orderbook_path[STOCK_TEST] ={{"/home/yzhengbv/00-data/git/hft-system/data/lobster/subset/AAPL_2012-06-21_34200000_57600000_orderbook_10.csv"},
+										{"/home/yzhengbv/00-data/git/hft-system/data/lobster/subset/AMZN_2012-06-21_34200000_57600000_orderbook_10.csv"},
+										{"/home/yzhengbv/00-data/git/hft-system/data/lobster/subset/GOOG_2012-06-21_34200000_57600000_orderbook_10.csv"}};
+	string result_path{"result.csv"};
+	string answer_path{"answer.csv"};
+	
+	// symbol
+	#ifndef __isLittleEndian__
+	char symbols[STOCK_TEST][8] =  {{'A','A','P','L',' ',' ',' ',' '},
+									{'A','M','Z','N',' ',' ',' ',' '},
+									{'G','O','O','G',' ',' ',' ',' '}};
+	#else
+	char symbols[STOCK_TEST][8] =  {{' ',' ',' ',' ','L','P', 'A','A'},
+									{' ',' ',' ',' ', 'N','Z','M','A'},
+									{' ',' ',' ',' ', 'G','O','O','G'}};
+	#endif
+
+	symbol_t *symbol_map=(symbol_t*)symbols;
+
+	// caches
+	last_manager* last_ls[STOCK_TEST];
+	long int cur_msg_time_ls[STOCK_TEST];
+	string last_orderbook_line_ls[STOCK_TEST], cur_msg_line_ls[STOCK_TEST];
+	ifstream message_ls[STOCK_TEST],  orderbook_ls[STOCK_TEST];
+	ofstream result, answer;
+
+	// order stream
+	int id=-1;
+
+	ap_uint<STOCK_TEST> neof = 0-1;
+
+	int get_symbol_loc(symbol_t target_symbol);
+	void close_files();
+
+	public:
+
+	vector<Message> init_book_messsages();
+	vector<Message> generate_messages(int num);
+	bool check_resultbook(vector<price_depth> price_stream_out, symbol_t target_symbol);
+	bool final_check();
+	
+	messageManager(){
+		// open files
+		for (int i=0; i<STOCK_TEST; i++){
+			// ifstream message, orderbook;
+			// message.open(message_path[i], ios::in);
+			// orderbook.open(orderbook_path[i], ios::in);
+			message_ls[i].open(message_path[i], ios::in);
+			orderbook_ls[i].open(orderbook_path[i], ios::in);
+			if((!message_ls[i])){
+				std::cout<<"Sorry the file you are looking for is not available: "<<message_path[i]<<endl;
+			}
+			// message_ls.push_back(&message);
+
+			if((!orderbook_ls[i])){
+				std::cout<<"Sorry the file you are looking for is not available: "<<orderbook_path[i]<<endl;
+			}
+			// orderbook_ls.push_back(&orderbook);
+		}
+
+		result.open(result_path, ios::out | ios::trunc);
+		answer.open(answer_path, ios::out | ios::trunc);
+	}
+
+};
