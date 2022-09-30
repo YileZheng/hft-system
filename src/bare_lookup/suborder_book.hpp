58bdc4866a5ef7f0a7a7f649db395a72d44732b0
Leon <leon@LeonsMacmini.local>
2022-09-30 16:47:04 +0800

    bare lookup table book coding completed

---
+++ src/bare_lookup/suborder_book.hpp
@@ -0,0 +1,20 @@
+#include <hls_math.h>
+#include <hls_stream.h>
+#include "common.hpp"
+#include "utils.hpp"
+
+
+#ifndef __SUBORDER_BOOK_H__
+#define __SUBORDER_BOOK_H__
+//#define __DEBUG__
+
+
+void dut_suborder_book(
+	orderMessage order_message,
+	ap_uint<8> read_max,
+	ap_uint<1> req_read_in,
+	hls::stream<price_depth> &feed_stream_out
+);
+
+
+#endif
