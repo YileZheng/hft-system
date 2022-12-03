6e2980765eff03f1e5144152094868001d769c73
Leon <yzhengbv@ust.hk>
2022-12-03 23:02:06 +0800

    make file update

--- build/order_book/transfer_result.sh
+++ build/order_book/transfer_result.sh
@@ -1,7 +1,7 @@
 set -e 
 set -u
 
-host=root@127.0.0.1
+host=root@143.89.131.98
 files="$host:/mnt/*summary* $host:/mnt/timeline_trace.csv"
 mkdir -p debug
-sshpass -p root scp -P 1440 -r $files ./debug
+sshpass -p root scp -P 10022 -r $files ./debug
